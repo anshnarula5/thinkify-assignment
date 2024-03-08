bab1d1366ace2a2d64076daf3a78ee0f5a8eb8d2
anshnarula5 <anshnarula55@gmail.com>
2024-03-08 19:56:45 +0530

    add constants

--- src/main/java/com/cabbooking/repository/DriverRepositoryImpl.java
+++ src/main/java/com/cabbooking/repository/DriverRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.cabbooking.repository;
 
 import com.cabbooking.model.Driver;
+import com.cabbooking.utils.Constants;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -28,6 +29,6 @@ public class DriverRepositoryImpl implements DriverRepository {
     private boolean isWithinDistance(final int[] driverLocation, final int[] source, final int[] destination) {
         double distance = Math.sqrt(Math.pow(driverLocation[0] - source[0], 2) +
                 Math.pow(driverLocation[1] - source[1], 2));
-        return distance <= 5;
+        return distance <= Constants.MAX_DISTANCE_LIMIT;
     }
 }
--- src/main/java/com/cabbooking/utils/Constants.java
+++ src/main/java/com/cabbooking/utils/Constants.java
@@ -1,5 +1,5 @@
 package com.cabbooking.utils;
 
 public class Constants {
-    public final Integer MAX_DISTANCE_LIMIT = 5;
+    public static final int MAX_DISTANCE_LIMIT = 5;
 }
